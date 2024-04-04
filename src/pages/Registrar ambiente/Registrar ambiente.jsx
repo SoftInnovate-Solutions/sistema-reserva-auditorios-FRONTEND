365c1a80d09666c94d09f06cb0ad2e23e889cb2e
JulioCRV <julioroquevalda6@gmail.com>
2024-04-04 01:21:39 -0400

    Validación de inputs y cuadro de texto con autocompletado

--- src/pages/Registrar ambiente/Registrar ambiente.jsx
+++ src/pages/Registrar ambiente/Registrar ambiente.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
@@ -6,23 +6,26 @@ import { Typography, useTheme } from '@mui/material';
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import { Grid } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
+// import { styled } from "@mui/material/styles";
+// import Paper from "@mui/material/Paper";
 
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: "center",
+//   color: theme.palette.text.secondary,
+// }));
 
 function RegistrarAmbiente() {
 
   const theme = useTheme();
-  const [tiposEdificios, setTiposEdificios] = useState([]);
 
+  //#region - LLAMADAS API Y CREACIÓN DE VARIABLES - CUADRO CON AUTOCOMPLETADO
+
+  // Creando variable para campos de entrada con autocompletado a través de llamadas API
+  const [tiposEdificios, setTiposEdificios] = useState([]);
   useEffect(() => {
     fetch('http://127.0.0.1:5000/edificio/all')
       .then(response => response.json())
@@ -31,7 +34,6 @@ function RegistrarAmbiente() {
   }, []);
 
   const [tiposFacultad, setTiposFacultad] = useState([]);
-
   useEffect(() => {
     fetch('http://127.0.0.1:5000/facultad/all')
       .then(response => response.json())
@@ -40,7 +42,6 @@ function RegistrarAmbiente() {
   }, []);
 
   const [tiposNumeroPiso, setTiposNumeroPiso] = useState([]);
-
   useEffect(() => {
     fetch('http://127.0.0.1:5000/piso/all')
       .then(response => response.json())
@@ -49,7 +50,6 @@ function RegistrarAmbiente() {
   }, []);
 
   const [tiposAmbiente, setTiposAmbiente] = useState([]);
-
   useEffect(() => {
     fetch('http://127.0.0.1:5000/tipo_ambiente/all')
       .then(response => response.json())
@@ -58,7 +58,6 @@ function RegistrarAmbiente() {
   }, []);
 
   const [tiposEstadoAmbiente, setTiposEstadoAmbiente] = useState([]);
-
   useEffect(() => {
     fetch('http://127.0.0.1:5000/estado_ambiente/all')
       .then(response => response.json())
@@ -67,6 +66,7 @@ function RegistrarAmbiente() {
   }, []);
 
 
+  // Crear una variable con una lista de etiquetas para cada campo de entrada con autocompletado.
   const opcionesTipoEdificio = tiposEdificios.map(tipo => ({
     label: tipo.nombre_edi,
   }));
@@ -87,18 +87,24 @@ function RegistrarAmbiente() {
   const opcionesEstadoAmbiente = tiposEstadoAmbiente.map(tipo => ({
     label: tipo.nombre_ea,
   }));
+  //#endregion
 
+  //#region - PARA CAPTURAR --> EL DATO <-- DE CAMPOS QUE EXISTE EN EL REGISTRO:
+
+  // Declarar variables para el cuadro de entrada con autocompletado
   const [contenidoTipoEdificio, setContenidoTipoEdificio] = useState(null);
   const [contenidoFacultad, setContenidoFacultad] = useState(null);
   const [contenidoNumeroPiso, setContenidoNumeroPiso] = useState(null);
   const [contenidoTipoAmbiente, setContenidoTipoAmbiente] = useState(null);
   const [contenidoEstadoAmbiente, setContenidoEstadoAmbiente] = useState(null);
 
+  // Declararar variables para los inputs
   const [nombreAmbiente, setNombreAmbiente] = useState('');
   const [descripcion, setDescripcion] = useState('');
   const [ubicacion, setUbicacion] = useState('');
   const [capacidad, setCapacidad] = useState('');
 
+  // Manejo de cambios en tiempo real en campos de entrada de texto.
   const manejadorCambiosNombreAmbiente = (event) => {
     setNombreAmbiente(event.target.value);
   };
@@ -112,55 +118,160 @@ function RegistrarAmbiente() {
   };
 
   const manejadorCambiosCapacidad = (event) => {
-    setCapacidad(event.target.value);
+    const inputNumero = event.target.value;
+    if (/^\d+$/.test(inputNumero)) {
+      setCapacidad(inputNumero);
+      setErrorCapacidad(false);
+      setMensajeErrorCapacidad('');
+    }else {
+      setErrorCapacidad(true);
+      setMensajeErrorCapacidad('Ingrese solo números');
+    }
   };
+  //#endregion
 
+  //#region - VALIDACIONES DE LOS CUADROS DE ENTRADA DE TEXTO 
 
+  // Declaracion de 4 tipos de errores y 4 tipos de mensaje de errores
+  const [errorAmbiente, setErrorAmbiente] = useState(false);
+  const [mensajeErrorAmbiente, setMensajeErrorAmbiente] = useState('');
+  const [errorDescripcion, setErrorDescripcion] = useState(false);
+  const [mensajeErrorDescripcion, setMensajeErrorDescripcion] = useState('');
+  const [errorUbicacion, setErrorUbicacion] = useState(false);
+  const [mensajeErrorUbicacion, setMensajeErrorUbicacion] = useState('');
+  const [errorCapacidad, setErrorCapacidad] = useState(false);
+  const [mensajeErrorCapacidad, setMensajeErrorCapacidad] = useState('');
 
+  // Validacion de los 4 inputs: devuelve true si no cumple la validacion
+  //                             devuelve false si cumple la validacion
+  const validarNombreAmbiente = () => {
+    if (!nombreAmbiente.trim()) {
+      setMensajeErrorAmbiente('El nombre del ambiente es requerido.');
+      setErrorAmbiente(true);
+      return true;
+    }
+
+    if (nombreAmbiente.trim().length > 20) {
+      setMensajeErrorAmbiente('El nombre de ambiente no debe exceder los 20 caracteres.');
+      setErrorAmbiente(true);
+      return true;
+    }
+    setMensajeErrorAmbiente('');
+    setErrorAmbiente(false);
+    return false;
+  }
+
+  const validarDescripcion = () => {
+    if (!descripcion.trim()) {
+      setMensajeErrorDescripcion('La descripción es requerido.');
+      setErrorDescripcion(true);
+      return true;
+    }
+
+    if (descripcion.trim().length > 255) {
+      setMensajeErrorDescripcion('La descripción no debe exceder los 255 caracteres');
+      setErrorDescripcion(true);
+      return true;
+    }
+    setMensajeErrorDescripcion('');
+    setErrorDescripcion(false);
+    return false;
+  }
+
+  const validarUbicacion = () => {
+    if (!ubicacion.trim()) {
+      setMensajeErrorUbicacion('La ubicación es requerida.');
+      setErrorUbicacion(true);
+      return true;
+    }
+
+    if (ubicacion.trim().length > 100) {
+      setMensajeErrorUbicacion('La ubicación no debe exceder los 100 caracteres');
+      setErrorUbicacion(true);
+      return true;
+    }
+    setMensajeErrorUbicacion('');
+    setErrorUbicacion(false);
+    return false;
+  }
+
+  const validarCapacidad = () => {
+    if (!capacidad.trim()) {
+      setMensajeErrorCapacidad('la capacidad es requerido.');
+      setErrorCapacidad(true);
+      return true;
+    }
+
+    if (capacidad.trim().length > 4) {
+      setMensajeErrorCapacidad('La capacidad no debe exceder los 4 cifras');
+      setErrorCapacidad(true);
+      return true;
+    }
+    setMensajeErrorCapacidad('');
+    setErrorCapacidad(false);
+    return false;
+  }
+
+  // Comprueba si la validacion es exitosa: retorna true si todos los inputs cumplen la restriccion establecida
+  // ! la negación: si al validar un campo nos devulve false -> cumple la validacion, !false -> la validacion es exitosa
+  const validarTodosLosInputs = () => {
+    validarNombreAmbiente(); validarDescripcion(); validarUbicacion(); validarCapacidad();
+    return !validarNombreAmbiente() && !validarDescripcion() && !validarUbicacion() && !validarCapacidad();
+  }
+  
+  //#endregion
+
+  //#region - IMPLEMENTACIÓN DE SELECCIÓN DE ID CON AUTOCOMPLETADO EN FORMULARIO PARA ENVIÓ A API
+
+  // Funcionalidad de captura de IDs en "campos de entrada con autocompletado" para luego enviar el formulario mediante una API a la base de datos
+  // en el formulario, solo se permiten IDs, por ejemplo: 1-> Laboratorio, 2-> Aulas, 3-> Auditorio.
   const obtenerIdCampoAutocompletado = (label) => {
     let codigoTipoAmbiente = null;
-  
+
     tiposEdificios.forEach(opcion => {
       if (opcion.nombre_edi === label) {
         codigoTipoAmbiente = opcion.cod_edificio;
-        return; // Sale del bucle forEach cuando se encuentra el valor
+        return;
       }
     });
-  
+
     tiposFacultad.forEach(opcion => {
       if (opcion.nombre_fac === label) {
         codigoTipoAmbiente = opcion.cod_facultad;
         return;
       }
     });
-  
+
     tiposNumeroPiso.forEach(opcion => {
       if (opcion.nombre_piso === label) {
         codigoTipoAmbiente = opcion.cod_piso;
         return;
       }
     });
-  
+
     tiposAmbiente.forEach(opcion => {
       if (opcion.nombre_ta === label) {
         codigoTipoAmbiente = opcion.cod_tipo_ambiente;
         return;
       }
     });
-  
+
     tiposEstadoAmbiente.forEach(opcion => {
       if (opcion.nombre_ea === label) {
         codigoTipoAmbiente = opcion.cod_estado_ambiente;
         return;
       }
     });
-  
+
     return codigoTipoAmbiente;
   };
-  
-  
+  //#endregion
 
+  //#region - INICIALIZACIÓN Y ENVIO DEL FORMULARIO
+
+  // Declaración, añadir varibales necesarias y uso de funcionalidad 
   const [formData, setFormData] = useState("");
+
   const rellenarDatos = () => {
     const datosAutocompletados = {
       nombre_amb: nombreAmbiente,
@@ -175,25 +286,52 @@ function RegistrarAmbiente() {
     };
 
     setFormData(datosAutocompletados);
-    
   };
+
+  // ENVIAR EL FORMDATA MEDIANTE UNA LLAMADA POST A LA BASE DE DATOS
+  const addEnAppi = async (e) => {
+
+  }
+  const manejarEnvioAux = () => {
+    //rellenarDatos();
+    console.log("validacion en general de todos: ",validarTodosLosInputs())
+    if (validarTodosLosInputs()) {
+      if(contenidoTipoAmbiente!=null && contenidoEstadoAmbiente!=null && contenidoTipoEdificio!=null &&
+         contenidoFacultad!=null && contenidoNumeroPiso){
+          console.log("++++++ => Formulario enviado");
+          console.log("Ambiente ", errorAmbiente, mensajeErrorAmbiente)
+          console.log("DEscripcion ", errorDescripcion, mensajeErrorDescripcion)
+          console.log("Ubicaciom", errorUbicacion, mensajeErrorUbicacion)
+          console.log("Capacida", errorCapacidad, mensajeErrorCapacidad)
+         }else{
+          console.log("CUADROS AUTOCOMPLETADOS OBLIGATORIOS")
+         }
+
+    } else {
+      console.log( "---------- => NAAAAAAAADA QUE ENVIAR")
+      console.log("Ambiente ", errorAmbiente, mensajeErrorAmbiente)
+      console.log("DEscripcion ", errorDescripcion, mensajeErrorDescripcion)
+      console.log("Ubicaciom", errorUbicacion, mensajeErrorUbicacion)
+      console.log("Capacida", errorCapacidad, mensajeErrorCapacidad)
+    }
+  }
 
   const manejarEnvio = async (e) => {
     e.preventDefault();
-
     try {
       rellenarDatos();
+      //console.log(JSON.stringify(datos),' <----> ', JSON.stringify(formData));
       const response = await fetch('http://127.0.0.1:5000/ambiente/add', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(formData)
-      });
 
+      });
+      //Manejo de exito o fracaso
       if (response.ok) {
         console.log('Ambiente creado exitosamente');
-        // Puedes redirigir a otra página o realizar otras acciones después de crear el ambiente
       } else {
         console.error('Error al crear el ambiente');
       }
@@ -201,6 +339,7 @@ function RegistrarAmbiente() {
       console.error('Error de red:', error);
     }
   };
+  //#endregion
 
   return (
     <div>
@@ -216,14 +355,17 @@ function RegistrarAmbiente() {
 
         <Typography variant="h5" component="h2" sx={{ mb: 5, color: theme.palette.text.primary }}>REGISTRAR AMBIENTE</Typography>
 
-        <form onSubmit={manejarEnvio}>
           <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 8, md: 3 }}>
             <Grid item xs={12} sm={6}>
+
               <TextField
                 sx={{ mb: 2, width: 300 }}
                 // id="nombreAmbiente"
                 value={nombreAmbiente}
                 onChange={manejadorCambiosNombreAmbiente}
+                onBlur={validarNombreAmbiente}
+                error={errorAmbiente}
+                helperText={errorAmbiente ? mensajeErrorAmbiente : ''}
                 label="Nombre de ambiente: "
                 variant="standard"
               />
@@ -231,20 +373,33 @@ function RegistrarAmbiente() {
                 sx={{ mb: 2, width: 300 }}
                 value={descripcion}
                 onChange={manejadorCambiosDescripcion}
+                onBlur={validarDescripcion}
+                error={errorDescripcion}
+                helperText={errorDescripcion ? mensajeErrorDescripcion : ''}
                 label="Descripción"
+                multiline
+                maxRows={3}
                 variant="standard"
               />
               <TextField
                 sx={{ mb: 2, width: 300 }}
                 value={ubicacion}
                 onChange={manejadorCambiosUbicacion}
+                onBlur={validarUbicacion}
+                error={errorUbicacion}
+                helperText={errorUbicacion ? mensajeErrorUbicacion : ''}
                 label="Ubicación: "
+                multiline
+                maxRows={2}
                 variant="standard"
               />
               <TextField
                 sx={{ mb: 2, width: 300 }}
                 value={capacidad}
                 onChange={manejadorCambiosCapacidad}
+                onBlur={validarCapacidad}
+                error={errorCapacidad}
+                helperText={errorCapacidad ? mensajeErrorCapacidad : ''}
                 label="Capacidad: "
                 variant="standard"
               />
@@ -319,10 +474,10 @@ function RegistrarAmbiente() {
               />
             </Grid>
           </Grid>
-          <Button type="submit" variant="contained" sx={{ width: "30%" }}>
+          <Button type="submit" variant="contained" onClick={manejarEnvioAux} sx={{ width: "30%" }}>
             REGISTRAR
           </Button>
-        </form>
+        
       </Box>
     </div>
   );
--- src/pages/pruebas.jsx
+++ src/pages/pruebas.jsx
@@ -1,48 +1,7 @@
-import React, { useState } from 'react';
-import { Button, TextField, Typography, Box } from '@mui/material';
-
-const FormularioRegistro = () => {
-  const [nombre, setNombre] = useState('');
-  const [mensaje, setMensaje] = useState('');
-
-  const handleNombreChange = (event) => {
-    setNombre(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (nombre.trim() !== '') {
-      setMensaje(`¡Hola, ${nombre}! Tu nombre ha sido registrado.`);
-    } else {
-      setMensaje('Por favor, ingresa tu nombre.');
-    }
-  };
+export default function pruebas() {
 
   return (
-    <Box sx={{ maxWidth: 400, margin: 'auto', textAlign: 'center' }}>
-      <Typography variant="h5" gutterBottom>
-        Registro de Nombre
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="Nombre"
-          variant="outlined"
-          fullWidth
-          value={nombre}
-          onChange={handleNombreChange}
-          sx={{ marginBottom: 2 }}
-        />
-        <Button type="submit" variant="contained" color="primary">
-          Registrar Nombre
-        </Button>
-      </form>
-      {mensaje && (
-        <Typography variant="body1" sx={{ marginTop: 2 }}>
-          {mensaje} 
-        </Typography>
-      )}
-    </Box>
+      <>
+      </>
   );
-};
-
-export default FormularioRegistro;
+}
